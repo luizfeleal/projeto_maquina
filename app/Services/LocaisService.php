271e0c6fe9c589cd2f50c2d6ca21ddab9408befb
Luiz Felipe <felipelearaujo@gmail.com>
2024-03-27 17:08:01 -0300

    Criação das função padrões do serviço

---
+++ app/Services/LocaisService.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Services;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Mail;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Http;
+
+
+
+class LocaisService
+{
+
+    public static function criar($dados){
+        $url = env('APP_URL_API') . "/locais";
+
+        $token = AuthService::getToken();
+        $response = Http::withHeaders([
+            'Authorization' => 'Bearer ' . $token
+        ])->post($url, $dados);
+
+        $locais = $response->json();
+
+        return $locais;
+    }
+
+    public static function coletar(string $id = Null)
+    {
+        if(is_null($id)){
+            $url = env('APP_URL_API') . "/locais";
+        }else{
+            $url = env('APP_URL_API') . "/locais/$id";
+        }
+        $token = AuthService::getToken();
+        $response = Http::withHeaders([
+            'Authorization' => 'Bearer ' . $token
+        ])->get($url);
+
+        $locais = $response->json();
+
+        return $locais;
+    }
+
+    public static function coletarComFiltro($filtros, $tipo)
+    {
+        $url = env('APP_URL_API') . "/extratoMaquina";
+
+        $token = AuthService::getToken();
+        $response = Http::withHeaders([
+            'Authorization' => 'Bearer ' . $token
+        ])->get($url);
+
+        if ($response->successful()) {
+            // Obtenha os clientes da resposta JSON
+            $locais = $response->json();
+
+            // Filtrar os clientes com base nos filtros fornecidos
+            foreach ($filtros as $chave => $valor) {
+                // Verifique se a chave existe e se o valor não está vazio
+                if (isset($locais[$chave]) && $valor !== null) {
+                    // Filtrar os clientes com base no valor do filtro
+                    $locais = array_filter($locais, function ($local) use ($chave, $valor) {
+                        return $local[$chave] === $valor;
+                    });
+                }
+            }
+
+            // Retorna os clientes filtrados
+            return $locais;
+        } else {
+            // Em caso de falha na chamada à API, retorne um array vazio ou uma mensagem de erro
+            return [];
+        }
+    }
+
+    public function atualizar($dados, $id){
+        $url = env('APP_URL_API') . "/extratoMaquina/$id";
+
+        $token = AuthService::getToken();
+        $response = Http::withHeaders([
+            'Authorization' => 'Bearer ' . $token
+        ])->post($url, $dados);
+
+        $local = $response->json();
+
+        return $local;
+    }
+
+}
