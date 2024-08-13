c3535661101d39721efca01c00274e3972f083f0
Luiz Felipe <felipelearaujo@gmail.com>
2024-08-12 21:26:25 -0300

    aplicando correção

--- app/Http/Controllers/MaquinasController.php
+++ app/Http/Controllers/MaquinasController.php
@@ -60,52 +60,74 @@ class MaquinasController extends Controller
         }
     }
 
-    public function coletarTodasAsMaquinas(Request $request){
+    public function coletarTodasAsMaquinas(Request $request) {
         $locais = LocaisService::coletar();
         $maquinas = MaquinasService::coletar();
         $maquinas_extrato = ExtratoMaquinaService::coletar();
-
+    
         // Indexando locais por id_local
         $locais_indexados = [];
         foreach ($locais as $local) {
             $locais_indexados[$local['id_local']] = $local;
         }
-
+    
         // Indexando maquinas por id_maquina
         $maquinas_indexadas = [];
         foreach ($maquinas as $maquina) {
             $maquinas_indexadas[$maquina['id_maquina']] = $maquina;
         }
-
+    
         // Array para armazenar o resultado final
         $resultado = [];
-
+    
         // Percorrendo o extrato para armazenar apenas a última transação de cada máquina
         foreach ($maquinas_extrato as $extrato) {
             $id_maquina = $extrato['id_maquina'];
-
+    
             // Verifica se a máquina existe
             if (isset($maquinas_indexadas[$id_maquina])) {
                 $maquina = $maquinas_indexadas[$id_maquina];
                 $id_local = $maquina['id_local'];
-
+    
                 // Verifica se o local existe
                 if (isset($locais_indexados[$id_local])) {
                     $local = $locais_indexados[$id_local];
-
+    
                     // Combina as informações
                     $extrato_completo = $extrato;
                     $extrato_completo['maquina'] = $maquina;
                     $extrato_completo['local'] = $local;
-
+    
                     // Armazena ou substitui a última transação da máquina no array de resultados
                     $resultado[$id_maquina] = $extrato_completo;
                 }
             }
         }
-
+    
+        // Verifica máquinas sem extrato e adiciona com transação zero
+        foreach ($maquinas_indexadas as $id_maquina => $maquina) {
+            if (!isset($resultado[$id_maquina])) {
+                $id_local = $maquina['id_local'];
+    
+                if (isset($locais_indexados[$id_local])) {
+                    $local = $locais_indexados[$id_local];
+    
+                    $resultado[$id_maquina] = [
+                        'id_maquina' => $id_maquina,
+                        'extrato_operacao_valor' => 0, // Define a última transação como 0
+                        'extrato_operacao' => "C",
+                        'extrato_operacao_tipo' => "N/A",
+                        'data_criacao' => $maquina['data_criacao'],
+                        'maquina' => $maquina,
+                        'local' => $local
+                    ];
+                }
+            }
+        }
+    
         // Se você quiser um array com índices numéricos simples, pode utilizar array_values
         $resultado = array_values($resultado);
+    
         return view('Admin.Maquinas.index', compact('resultado'));
     }
 
