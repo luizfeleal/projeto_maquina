2345516cedd181fd5cff35ed3c92cb58fe612acd
Luiz Felipe <felipelearaujo@gmail.com>
2024-09-06 16:46:05 -0300

    Script para exibição de usuários que não estão presentes no local

--- resources/views/Admin/Local/index.blade.php
+++ resources/views/Admin/Local/index.blade.php
@@ -183,9 +183,9 @@
 
 
             var tabelaGuias= $('#tabela-local').DataTable({
-                /*"language": {
-                    "url": "//cdn.datatables.net/plug-ins/1.13.6/i18n/pt-BR.json"
-                },*/
+                "language": {
+                    "url": "https://cdn.datatables.net/plug-ins/1.13.6/i18n/pt-BR.json"
+                },
                 "columns": [
                     null,
                     null,
