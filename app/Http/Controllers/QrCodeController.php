194541ce34ae8c1e7c794c7ba2866184d04c44e6
Luiz Felipe <felipelearaujo@gmail.com>
2024-09-11 12:46:23 -0300

    correção de erros

--- app/Http/Controllers/QrCodeController.php
+++ app/Http/Controllers/QrCodeController.php
@@ -90,7 +90,6 @@ class QrCodeController extends Controller
             $cliente_local = array_filter($cliente_local, function($item) use($id_local){
                 return $item['id_local'] == $id_local && $item['cliente_local_principal'] == 1;
             });
-            return $cliente_local;
 
             if(empty($cliente_local)){
                 return back()->with('error', 'Não foi possível gerar um QR para os dados passados, pois não foi encontrado um cliente para o local especificado.');
