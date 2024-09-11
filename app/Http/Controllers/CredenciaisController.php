7060bf6760b8015a115ddc977419bdf0aff7acad
Luiz Felipe <felipelearaujo@gmail.com>
2024-09-11 12:24:00 -0300

    correção de erros

--- app/Http/Controllers/CredenciaisController.php
+++ app/Http/Controllers/CredenciaisController.php
@@ -40,9 +40,11 @@ class CredenciaisController extends Controller
 
             $credencial = CredApiPixService::coletar();
 
+            return $credencial;
             $credencial_existente = array_filter($credencial, function($item) use($request){
                 return $item['id_cliente'] == $request['id_cliente'] && $item['tipo_cred'] == $request['tipo_cred'];
             });
+
 
             
             if(!empty($credencial_existente)){
