bda6d96fb968b2835a2ef3a198a44d37cbee1022
Luiz Felipe <felipelearaujo@gmail.com>
2024-09-11 12:51:01 -0300

    correção de erros

--- app/Http/Controllers/QrCodeController.php
+++ app/Http/Controllers/QrCodeController.php
@@ -97,12 +97,14 @@ class QrCodeController extends Controller
             }
             $cliente_local = array_values($cliente_local);
 
+            $id_cliente = $cliente_local[0]['id_cliente'];
+
             $credenciais = CredApiPixService::coletar();
 
-            return $credenciais;
-            $credencial = array_filter($credenciais, function($item) use($cliente_local){
-                return $item['id_cliente'] == $cliente_local[0]['id_cliente'] && $item['tipo_cred'] == "efi";
+            $credencial = array_filter($credenciais, function($item) use($id_cliente){
+                return $item['id_cliente'] == $id_cliente && $item['tipo_cred'] == "efi";
             });
+            return $credencial;
 
             if(empty($credencial)){
                 return back()->with('error', 'Não foi possível gerar um QR para os dados passados, pois não foi encontrado uma credencial para o cliente informado');
