c80fb0d0b11df784f43df631b7e7150c21a363af
Luiz Felipe <felipelearaujo@gmail.com>
2024-11-21 19:32:26 -0300

    modificações seguindo o trello

--- resources/views/Admin/home.blade.php
+++ resources/views/Admin/home.blade.php
@@ -74,7 +74,7 @@
                         <td>{{$maquina['id_placa']}}</td>
                         <td>{{$maquina['maquina_nome']}}</td>
                         <td style="text-align: center;"><a href="/qr?id_local={{$maquina['id_local']}}&id_maquina={{$maquina['id_maquina']}}"><i class="fa-solid fa-qrcode icon-sidebar"></i></a></td>
-                        <td style="text-align: center;"><a href="/maquinas/liberarJogada?id_maquina={{$id_maquina}}" ><i class="fa-solid fa-play icon-sidebar"></i></a></td>
+                        <td style="text-align: center;"><a href="/maquinas/liberarJogada?id_maquina={{$maquina['id_maquina']}}" ><i class="fa-solid fa-play icon-sidebar"></i></a></td>
                         <td style="text-align: center;"><a href="/maquinas/editar?id_maquina={{$maquina['id_maquina']}}"><i class="fa-solid fa-pen"></i></a></td>
                     </tr>
                     @endforeach
