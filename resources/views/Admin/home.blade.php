55cac82e7c9f29ebda7b3f7f95fca6eb34f12052
Luiz Felipe <felipelearaujo@gmail.com>
2024-11-25 21:41:57 -0300

    modificações seguindo o trello

--- resources/views/Admin/home.blade.php
+++ resources/views/Admin/home.blade.php
@@ -159,6 +159,7 @@
             "language": {
                 "url": "https://cdn.datatables.net/plug-ins/1.13.6/i18n/pt-BR.json"
             },
+            "scrollX": true,
             "columns": [
                 null,
                 null,
