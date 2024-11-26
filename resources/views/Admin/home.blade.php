73573726e30ff1c3b3826e20482044a4f22fc181
Luiz Felipe <felipelearaujo@gmail.com>
2024-11-25 21:40:44 -0300

    modificações seguindo o trello

--- resources/views/Admin/home.blade.php
+++ resources/views/Admin/home.blade.php
@@ -16,7 +16,7 @@
                 <div class="card" style="height: 100%;">
                     <h5 class="card-header">Saldo Disponível</h5>
                     <div class="card-body" style="display: flex; justify-content: center; align-items: center;">
-                        <p class="card-text">R$ {{number_format($saldo['data'], 2, ',', '.')}}</p>
+                        <p class="card-text"> <strong>R$ {{number_format($saldo['data'], 2, ',', '.')}}</strong></p>
                     </div>
                 </div>
             </div>
