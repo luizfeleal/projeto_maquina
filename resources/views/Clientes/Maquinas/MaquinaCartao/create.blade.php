f952c60aff441278d80f491bedba457b77104718
Luiz Felipe <felipelearaujo@gmail.com>
2024-12-21 14:54:04 -0300

    Corrigindo ordenação de tabela

---
+++ resources/views/Clientes/Maquinas/MaquinaCartao/create.blade.php
@@ -0,0 +1,123 @@
+@extends('layouts.Clientes.app')
+@section('title', 'Incluir Máquina Cartão')
+@section('content')
+
+        <div id="local-incluir-usuario" class="local div-center-column w-100"
+                style="padding-top: 99px;">
+
+                <h1  style="padding-top: 80px; text-align: center;">Incluir Máquina Cartão</h1>
+            <div class="container section container-platform div-center-column"
+                style="margin-top: 15px; height: 100%;">
+
+
+                <form action="{{ route('cliente-maquinas-cartao-registrar') }}" method="post" id="incluir_usuario_local_form" class="w-100">
+                    @csrf
+
+                    <div class="row" style="display: flex; flex-direction: row; justify-content: center;width: 100%; margin-bottom: 20px;">
+                        <div class="col-md-6">
+                            <p >Selecione a máquina que deseja incluir a máquina de cartão:</p>
+                            <label for="maquina" class="form-label">Máquina*:</label>
+                            <select class="select-maquina js-example-basic-multiple js-states form-control" id="select-maquina" placeholder="Selecione" name="select-maquina">
+
+                            <option value="" selected>Selecione</option>
+                            @foreach($maquinas_exibir as $maquina)
+                                <option value="{{$maquina['id_maquina']}}">{{$maquina['maquina_nome']}}</option>
+                            @endforeach
+                            </select>
+                            <div class="invalid-feedback">
+                                <p class="invalid-p" id="select_local_mensagem">Campo obrigatório</p>
+                            </div>
+
+                        </div>
+                    </div>
+                    <div class="row" style="display: flex; flex-direction: row; justify-content: center;  width: 100%; ">
+                        <div class="col-md-6">
+                            <label for="device" class="form-label">Insira o número da máquina de cartão*:</label>
+                            <input type="text" id="device" name="device" class="form-control input-text" placeholder="Número" aria-label="Número" required>
+                            <div class="invalid-feedback">
+                                <p class="invalid-p" id="select_cliente_mensagem">Campo obrigatório</p>
+                            </div>
+
+                        </div>
+                    </div>
+
+                    <div style="display:flex; justify-content: center; align-items: center; margin-top: 50px;">
+                        <button class="btn btn-primary"  type="submit">Cadastrar</button>
+                    </div>
+                </form>
+
+                    <div class="modal fade" id="ModalCenterCriar" tabindex="-1" aria-labelledby="ModalCenterCriar" aria-modal="true" role="dialog">
+                        <div class="modal-dialog modal-dialog-centered">
+                            <div class="modal-content">
+
+                                <div class="modal-header">
+                                    <h1 class="modal-title fs-5" id="ModalCenterTitleCriar">Criar</h1>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+                                    <p>Realmente deseja incluir o(s) usuário(s) no local? Ao incluir, o usuário verá todas as informações deste local.</p>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secundary" data-bs-dismiss="modal" aria-label="Close">Cancelar</button>
+                                    <button type="submit" class="btn btn-primary" data-bs-dismiss="modal" aria-label="Close" onclick="sendFormIncluirUsuarioLocal()">Sim</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                            @if(session('success'))
+
+                                <div class="modal fade show" id="modalSuccess" tabindex="-1" aria-labelledby="exampleModalCenterTitle" aria-modal="true" role="dialog" style="display: block;">
+                                    <div class="modal-dialog modal-dialog-centered">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h1 class="modal-title fs-5" id="exampleModalCenterTitle">Sucesso</h1>
+                                                <button type="button" class="btn-close" onclick="fechaModal('modalSuccess')" aria-label="Close"></button>
+                                            </div>
+                                            <div class="modal-body">
+                                                <p>{{ session('success') }}</p>
+                                            </div>
+                                            <div class="modal-footer">
+                                                <button type="button" class="btn btn-primary" onclick="fechaModal('modalSuccess')" data-dismiss="modal" aria-label="Close">OK</button>
+                                            </div>
+
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="modal-backdrop fade show" id="modalSuccess-backdrop"></div>
+                            @elseif(session('error'))
+                                <div class="modal fade show" id="modalError" tabindex="-1" aria-labelledby="exampleModalCenterTitle" aria-modal="true" role="dialog" style="display: block;">
+                                        <div class="modal-dialog modal-dialog-centered">
+                                            <div class="modal-content">
+
+                                                <div class="modal-header">
+                                                    <h1 class="modal-title fs-5" id="exampleModalCenterTitle">Erro</h1>
+                                                    <button type="button" class="btn-close" onclick="fechaModal('modalError')" data-bs-dismiss="modal" aria-label="Close"></button>
+                                                </div>
+                                                <div class="modal-body">
+                                                    <p>{{ session('error') }}</p>
+                                                </div>
+                                                <div class="modal-footer">
+                                                        <button type="button" class="btn btn-primary" onclick="fechaModal('modalError')" data-bs-dismiss="modal" aria-label="Close">OK</button>
+                                                </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="modal-backdrop fade show" id="modalError-backdrop"></div>
+                            @endif
+            </div>
+        </div>
+
+@endsection
+
+@section('scriptTable')
+
+<script>
+    $(document).ready(function() {
+        $('#select-maquina').select2({
+            theme: 'bootstrap-5'
+        });
+    });
+</script>
+
+@endsection
+
---
+++ resources/views/Clientes/Maquinas/MaquinaCartao/index.blade.php
@@ -0,0 +1,180 @@
+@extends('layouts.Clientes.app')
+@section('title', 'Maquina de Cartão')
+@section('content')
+
+        <div id="guias" class="usuarios w-100 div-center-column"
+                style=" padding-top: 99px; padding-bottom: 100px;">
+
+                <h1 style="padding-top: 80px; text-align: center; padding-bottom: 50px;">Máquina de Cartão</h1>
+
+            <div class="container section container-platform div-center-column"
+                style="margin-top: 15px; height: 100%;">
+                <div class="row" style="display: flex; flex-direction: row; justify-content: center; margin-bottom: 20px; width: 100%;">
+                    <div class="col-md-4">
+                    
+                    </div>
+                    <div class="col-md-4" style="display: flex; flex-direction: row; justify-content: end; align-items: center;">
+                        <a  href="{{route('cliente-maquinas-cartao-criar')}}"><button class="btn btn-primary">Nova Máquina <i class="fa-solid fa-plus"></i></button></a>
+                    </div>
+                </div>
+
+                <div class="tabela_responsiva">
+
+                    <table id="tabela-local" class="table table-striped" style="width:100%">
+                        <thead>
+                            <tr>
+                                <th>Nome Máquina</th>
+                                <th>Número Máquina</th>
+                                <th>Status</th>
+                                <th>Ativar/Inativar</th>
+    
+    
+                            </tr>
+                        </thead>
+                        <tbody>
+    
+                        
+                        @foreach($maquinasCartao as $maquina)
+                            <tr>
+                                <td>{{$maquina['maquina_nome']}}</td>
+                                <td>{{$maquina['device']}}</td>
+                                <td>{{$maquina['status'] == 1 ? "Ativo" : "Inativo"}}</td>
+
+
+                                @if($maquina['status'] == 1)
+                                <td style="text-align: center;"><a href="#" style="color: red !important;" data-bs-toggle="modal" data-bs-target="#ModalCenterExcluir" onclick="setIdMaquinaCartaoExcluir({{$maquina['id']}}, '#id_device_inativar')"><i class="fa-solid fa-ban"></i></a></td>
+                                @else
+                                <td style="text-align: center;"><a href="#" style="color: green !important;" data-bs-toggle="modal" data-bs-target="#ModalCenterAtivar" onclick="setIdMaquinaCartaoExcluir({{$maquina['id']}}, '#id_device_ativar')"><i class="fa-solid fa-circle-check"></i></a></td>
+                                @endif
+                            </tr>
+                        @endforeach
+    
+    
+                        </tbody>
+                        <tfoot>
+                            <tr>
+                                <th>Nome Máquina</th>
+                                <th>Número Máquina</th>
+                                <th>Status</th>
+                                <th>Ativar/Inativar</th>
+                            </tr>
+                        </tfoot>
+                    </table>
+                </div>
+
+
+                <div class="modal fade" id="ModalCenterExcluir" tabindex="-1" aria-labelledby="ModalCenterExcluir" aria-modal="true" role="dialog">
+                        <div class="modal-dialog modal-dialog-centered">
+                            <div class="modal-content">
+
+                                <div class="modal-header">
+                                    <h1 class="modal-title fs-5" id="ModalCenterTitleExcluir">Inativar</h1>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+                                    <p>Deseja inativar essa máquina de cartão?</p>
+                                </div>
+                                <div class="modal-footer">
+                                    <form action="{{route('maquinas-cartao-inativar')}}" method="post" id="excluir-cliente" class="w-100 " >
+                                    @csrf
+                                        <input type="hidden" name="id_device" id="id_device_inativar" value="" >
+                                        <input type="hidden" name="status" id="status_device" value="0" >
+                                        <button type="submit" class="btn btn-primary" data-bs-dismiss="modal" aria-label="Close" >Sim</button>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="modal fade" id="ModalCenterAtivar" tabindex="-1" aria-labelledby="ModalCenterAtivar" aria-modal="true" role="dialog">
+                        <div class="modal-dialog modal-dialog-centered">
+                            <div class="modal-content">
+
+                                <div class="modal-header">
+                                    <h1 class="modal-title fs-5" id="ModalCenterTitleAtivar">Inativar</h1>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+                                    <p>Deseja ativar essa máquina de cartão?</p>
+                                </div>
+                                <div class="modal-footer">
+                                    <form action="{{route('maquinas-cartao-inativar')}}" method="post" id="excluir-cliente" class="w-100 " >
+                                    @csrf
+                                        <input type="hidden" name="id_device" id="id_device_ativar" value="" >
+                                        <input type="hidden" name="status" id="status_device" value="1" >
+                                        <button type="submit" class="btn btn-primary" data-bs-dismiss="modal" aria-label="Close" >Sim</button>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                            @if(session('success'))
+
+                                <div class="modal fade show" id="modalSuccess" tabindex="-1" aria-labelledby="exampleModalCenterTitle" aria-modal="true" role="dialog" style="display: block;">
+                                    <div class="modal-dialog modal-dialog-centered">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h1 class="modal-title fs-5" id="exampleModalCenterTitle">Sucesso</h1>
+                                                <button type="button" class="btn-close" onclick="fechaModal('modalSuccess')" aria-label="Close"></button>
+                                            </div>
+                                            <div class="modal-body">
+                                                <p>{{ session('success') }}</p>
+                                            </div>
+                                            <div class="modal-footer">
+                                                <button type="button" class="btn btn-primary" onclick="fechaModal('modalSuccess')" data-dismiss="modal" aria-label="Close">OK</button>
+                                            </div>
+
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="modal-backdrop fade show" id="modalSuccess-backdrop"></div>
+                            @elseif(session('error'))
+                                <div class="modal fade show" id="modalError" tabindex="-1" aria-labelledby="exampleModalCenterTitle" aria-modal="true" role="dialog" style="display: block;">
+                                        <div class="modal-dialog modal-dialog-centered">
+                                            <div class="modal-content">
+
+                                                <div class="modal-header">
+                                                    <h1 class="modal-title fs-5" id="exampleModalCenterTitle">Erro</h1>
+                                                    <button type="button" class="btn-close" onclick="fechaModal('modalError')" data-bs-dismiss="modal" aria-label="Close"></button>
+                                                </div>
+                                                <div class="modal-body">
+                                                    <p>{{ session('error') }}</p>
+                                                </div>
+                                                <div class="modal-footer">
+                                                        <button type="button" class="btn btn-primary" onclick="fechaModal('modalError')" data-bs-dismiss="modal" aria-label="Close">OK</button>
+                                                </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="modal-backdrop fade show" id="modalError-backdrop"></div>
+                            @endif
+            </div>
+        </div>
+
+
+
+
+@endsection
+
+@section('scriptTable')
+    <script>
+
+        $(document).ready(function(){
+
+
+            var tabelaGuias= $('#tabela-local').DataTable({
+                "language": {
+                    "url": "https://cdn.datatables.net/plug-ins/1.13.6/i18n/pt-BR.json"
+                },
+                "scrollX": true,
+                "columns": [
+                    null,
+                    null,
+                    null,
+                    null,
+                ] // Use o array de objetos de coluna dinamicamente criado
+            });
+        });
+    </script>
+
+@endsection
+
