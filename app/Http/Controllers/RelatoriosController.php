9bc98331e7234b11a895b25c55edf7937e7d05be
Luiz Felipe <felipelearaujo@gmail.com>
2025-01-15 15:30:09 -0300

    Corrigindo relatório de erros

--- app/Http/Controllers/RelatoriosController.php
+++ app/Http/Controllers/RelatoriosController.php
@@ -161,7 +161,7 @@ class RelatoriosController extends Controller
             $resultadosFiltrados = [];
             
             foreach ($relatorioDeErros as &$item) {
-                return $maquinas[$item['id_maquina']];
+
 
                 $item['id_placa'] = isset($maquinas[$item['id_maquina']]['id_placa']) ? $maquinas[$item['id_maquina']]['id_placa'] : '';
                 $dataItem = strtotime($item["data_criacao"]);
