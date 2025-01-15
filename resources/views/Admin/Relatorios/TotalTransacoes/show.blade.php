2e356027636e436018045d48fc80e07f7b0feaa0
Luiz Felipe <felipelearaujo@gmail.com>
2025-01-14 22:04:56 -0300

    Corrigindo a quantidade de caracteres do TXID

--- resources/views/Admin/Relatorios/TotalTransacoes/show.blade.php
+++ resources/views/Admin/Relatorios/TotalTransacoes/show.blade.php
@@ -134,7 +134,6 @@
 @section('scriptTable')
 <script>
 $(document).ready(function () {
-    console.log(@json($data_extrato_inicio))
     // Definir o tipo de ordenação personalizada para datas no formato dd/mm/yyyy
     $.fn.dataTable.ext.type.order['datetime-ddmmyyyy-pre'] = function (d) {
         if (d === 'Data não disponível' || !d) {
